0b6c97c6fed4e2029daec276a118aed23d82c5d1
Patryk Czekaj <patryk13290@gmail.com>
2025-11-28 18:35:10 +0100

    hide only from front

--- Scripts/desk.gd
+++ Scripts/desk.gd
@@ -5,9 +5,14 @@ extends StaticBody3D
 func get_interact_text() -> String:
 	if Globals.current_map.player.hidden:
 		return "[E] stop hiding"
-	return "[E] hide"
+	elif get_angle_to_player() < 40:
+		return "[E] hide"
+	
+	return ""
 
 func interact():
+	if get_angle_to_player() > 40:
+		return
 	var sec_cam := Globals.current_map.secondary_camera
 	sec_cam.get_parent().remove_child(sec_cam)
 	hide_location.add_child(sec_cam)
@@ -17,3 +22,13 @@ func interact():
 	sec_cam.current = true
 	Globals.current_map.player.hidden = true
 	Globals.current_map.player.drop_item()
+	
+func get_angle_to_player():
+		var pos := hide_location.global_position
+		pos.y = 0
+		var pos_p := Globals.current_map.player.global_position
+		pos_p.y = 0
+		var dir = (pos_p - pos).normalized()
+		var forward = -hide_location.global_basis.z
+		return rad_to_deg(acos(forward.dot(dir)))
+	
