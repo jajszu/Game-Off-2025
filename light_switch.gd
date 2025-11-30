7c5a79ec017ad83468ee4caa974be3de698016d5
jajszu <bartekjaje@gmail.com>
2025-11-30 23:19:04 +0100

    trash spawn and lights during chase

--- light_switch.gd
+++ light_switch.gd
@@ -4,7 +4,26 @@ extends StaticBody3D
 
 func _ready() -> void:
 	for l in lights:
-		l.light_energy = 15
+		l.light_energy = 12
+
+func _process(delta: float) -> void:
+	if Globals.current_ghost != null:
+		if not flicker_in_progress:
+			flicker_lights()
+	else:
+		for l in lights:
+			l.light_energy = 12
+
+
+var flicker_in_progress := false
+
+func flicker_lights() -> void:
+	flicker_in_progress = true
+	for l in lights:
+		l.light_energy = 5
+		await get_tree().create_timer(randf_range(0.2,0.4)).timeout
+	flicker_in_progress = false
+
 
 func get_interact_text() -> String:
 	return "[E] toggle light"
